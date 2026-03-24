a02e550b767d143aac2eec77e74b4b061e3396b3
ATMunn <iamingodsarmy@gmail.com>
2026-03-24 14:04:13 -0400

    Fleshed out the UI a bit more

---
+++ src/components/GroupMember.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Field } from '@/components/ui/field'
+import {
+  TagsInput,
+  TagsInputItem,
+  TagsInputItemText,
+  TagsInputItemDelete,
+  TagsInputInput,
+} from '@/components/ui/tags-input'
+import { Button } from '@/components/ui/button'
+import { Trash } from 'lucide-vue-next'
+
+const airports = defineModel<string[]>('airports')
+defineProps<{
+  deleteMember: () => void
+}>()
+</script>
+
+<template>
+  <div class="flex w-full gap-1 items-center">
+    <Field>
+      <TagsInput v-model="airports" add-on-tab :delimiter="/[ ,]/">
+        <TagsInputItem v-for="airport in airports" :key="airport" :value="airport">
+          <TagsInputItemText :text="airport" />
+          <TagsInputItemDelete />
+        </TagsInputItem>
+        <TagsInputInput placeholder="Enter airport code..." />
+      </TagsInput>
+    </Field>
+    <Button
+      variant="ghost"
+      size="icon"
+      @click="deleteMember"
+      class="cursor-pointer text-red-500 hover:bg-red-500/80"
+      ><Trash
+    /></Button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,55 @@
 <script setup lang="ts">
-import { Field, FieldLabel, FieldDescription } from '@/components/ui/field'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-} from '@/components/ui/card'
-import {
-  TagsInput,
-  TagsInputInput,
-  TagsInputItem,
-  TagsInputItemText,
-  TagsInputItemDelete,
-} from '@/components/ui/tags-input'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { ref } from 'vue'
-import { Plane } from 'lucide-vue-next'
+import { Plane, Plus } from 'lucide-vue-next'
+import GroupMember from '@/components/GroupMember.vue'
 
-const airports = ref<string[]>([])
+const groupMembers = ref<string[][]>([])
 </script>
 
 <template>
   <div class="flex flex-col h-screen w-screen items-center justify-center">
     <h1 class="text-3xl font-bold mb-4">Middle Ground</h1>
-    <Card class="w-full max-w-md">
-      <CardHeader>
-        <CardTitle>Welcome!</CardTitle>
-        <CardDescription>To start, enter the home airport codes.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Field>
-          <FieldLabel for="airports">Airports</FieldLabel>
-          <TagsInput v-model="airports" add-on-tab :delimiter="/[, ]/">
-            <TagsInputItem v-for="airport in airports" :key="airport" :value="airport">
-              <TagsInputItemText :text="airport" />
-              <TagsInputItemDelete />
-            </TagsInputItem>
-            <TagsInputInput placeholder="TOL, DTW, ..." />
-          </TagsInput>
-          <FieldDescription>Enter the airport codes</FieldDescription>
-        </Field>
-      </CardContent>
-      <CardFooter class="justify-end">
-        <Button :disabled="airports.length < 2" class="cursor-pointer"
-          ><Plane />Begin Search</Button
-        >
-      </CardFooter>
-    </Card>
+    <div class="w-full flex justify-center gap-4">
+      <Card class="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Welcome!</CardTitle>
+          <CardDescription>To start, enter the home airport codes.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <h2 class="font-bold mb-2">Group Members</h2>
+          <div class="flex flex-col gap-2 bg-secondary rounded-md p-2">
+            <div v-if="groupMembers.length === 0">
+              <p class="text-muted-foreground text-sm">No group members added</p>
+            </div>
+            <GroupMember
+              v-for="(member, index) in groupMembers"
+              :key="index"
+              v-model:airports="groupMembers[index]"
+              :delete-member="() => groupMembers.splice(index, 1)"
+            />
+          </div>
+          <Button
+            class="cursor-pointer mt-2"
+            size="sm"
+            variant="outline"
+            @click="groupMembers.push([])"
+            ><Plus />Add Group Member</Button
+          >
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Preferences</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button>something</Button>
+        </CardContent>
+      </Card>
+    </div>
+    <Button :disabled="groupMembers.length < 2" class="cursor-pointer mt-4"
+      ><Plane />Begin Search</Button
+    >
   </div>
 </template>
