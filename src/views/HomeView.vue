f4e9f208cdd0591a9894e8e26e58090bb218cd71
ATMunn <iamingodsarmy@gmail.com>
2026-04-07 15:04:56 -0400

    preferences panel (mockup)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,27 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { ref } from 'vue'
-import { Plane, Plus } from 'lucide-vue-next'
+import {
+  Plane,
+  Plus,
+  Sun,
+  SunDim,
+  Thermometer,
+  Wind,
+  Snowflake,
+  Droplet,
+  DropletOff,
+  CloudSun,
+  CloudRain,
+  TreePalm,
+  Mountain,
+  Building2,
+  Waves,
+  Icon,
+  Droplets,
+  Star,
+} from '@lucide/vue'
+import { cactus, palmtreeIslandSun } from '@lucide/lab'
 import GroupMember from '@/components/GroupMember.vue'
 
 const groupMembers = ref<string[][]>([])
@@ -41,10 +61,58 @@ const groupMembers = ref<string[][]>([])
       </Card>
       <Card>
         <CardHeader>
-          <CardTitle>Preferences</CardTitle>
+          <CardTitle class="text-lg">Preferences</CardTitle>
         </CardHeader>
-        <CardContent>
-          <Button>something</Button>
+        <CardContent class="flex flex-col gap-2">
+          <h1 class="font-bold">Weather</h1>
+          <div class="px-4 pt-2 pb-4 bg-secondary rounded-md">
+            <h2 class="font-semibold mb-1 text-sm">Temperature</h2>
+            <div class="grid grid-cols-5 gap-2">
+              <Button variant="outline"><Sun />Hot</Button>
+              <Button variant="outline"><SunDim />Warm</Button>
+              <Button variant="outline"><Thermometer />Mild</Button>
+              <Button variant="outline"><Wind />Cool</Button>
+              <Button variant="outline"><Snowflake />Cold</Button>
+            </div>
+          </div>
+          <div class="px-4 pt-2 pb-4 bg-secondary rounded-md">
+            <h2 class="font-semibold mb-1 text-sm">Conditions</h2>
+            <div class="grid grid-cols-5 gap-2">
+              <Button
+                variant="outline"
+                class="border-sky-500 border-3 bg-sky-100 relative hover:bg-sky-200 cursor-pointer group"
+              >
+                <Sun />Sunny
+                <div
+                  class="absolute -right-1 -top-1 bg-sky-200/90 text-sky-600 rounded-full p-1 group-hover:bg-sky-300/90 group-hover:text-sky-600 transition-all duration-200"
+                >
+                  <Star />
+                </div>
+              </Button>
+              <Button variant="outline"><Icon :iconNode="cactus" />Arid</Button>
+              <Button variant="outline"><CloudRain />Rainy</Button>
+              <Button variant="outline" class="border-amber-500 border-3 bg-amber-100 relative">
+                <DropletOff />Dry
+                <div
+                  class="absolute -right-1 -top-1 bg-amber-200/90 text-amber-600 rounded-full p-1"
+                >
+                  <Star fill="currentColor" />
+                </div>
+              </Button>
+              <Button variant="outline"><Droplets />Humid</Button>
+            </div>
+          </div>
+          <h1 class="font-bold">Geography</h1>
+          <div class="px-4 pt-2 pb-4 bg-secondary rounded-md">
+            <h2 class="font-semibold mb-1 text-sm">Environment</h2>
+            <div class="grid grid-cols-5 gap-2">
+              <Button variant="outline"><TreePalm />Beach</Button>
+              <Button variant="outline"><Waves />Coastal</Button>
+              <Button variant="outline"><Icon :iconNode="palmtreeIslandSun" />Island</Button>
+              <Button variant="outline"><Building2 />Urban</Button>
+              <Button variant="outline"><Mountain />Mountains</Button>
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>
